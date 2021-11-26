ca1f473e6876738c7ecb0dd72c424838c14c26b9
Richard <monaroau@yahoo.com>
2021-11-26 18:34:03 +1100

    added weather to the mix

--- app/python/examples/weather.py
+++ app/python/examples/weather.py
@@ -13,12 +13,17 @@ import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 import random
+import requests
+import ftplib
+import xml.etree.ElementTree as ET
+import textwrap
 
 logging.basicConfig(level=logging.DEBUG)
-
-try:
-    logging.info("Swimerator started")
     
+try:
+
+    logging.info("Blackheath weather started")
+
     # initialise the epaper
     epd = epd3in7.EPD()
     logging.info("init and Clear")
@@ -26,6 +31,40 @@ try:
     epd.Clear(0xff, 0)
 
     # set some variables
+    # import some data from the BOM
+    x = requests.get("http://www.bom.gov.au/fwo/IDN60801/IDN60801.94743.json", headers={'User-Agent': 'My User Agent 1.0'})
+    jsonData = x.json()
+    temp = jsonData["observations"]["data"][0]["air_temp"]
+    feelsLike = jsonData["observations"]["data"][0]["apparent_t"]
+    lastChecked = jsonData["observations"]["data"][0]["local_date_time"]
+
+    # get forecast data from ftp and parse the xml
+    FTP_host = "ftp.bom.gov.au"
+    FTP = ftplib.FTP()
+    FTP.connect(FTP_host)
+    FTP.login()
+    FTP.cwd("/anon/gen/fwo/") # navigate to correct directory
+    FTP.retrbinary("RETR IDN11109.xml", open('myxml.xml', 'wb').write)
+
+    minTemp = '-'
+    maxTemp = '-'
+    forecast = '-'
+    root = ET.parse('myxml.xml').getroot()
+    latestElement = root[1][1][0]
+    dataElements = latestElement.findall('element')
+    dataTexts = latestElement.findall('text')
+ 
+    for dataElement in dataElements:
+        if(dataElement.get('type') == 'air_temperature_minimum'):
+            minTemp = dataElement.text
+        if(dataElement.get('type') == 'air_temperature_maximum'):
+            maxTemp = dataElement.text
+        
+    for dataText in dataTexts:
+        if(dataText.get('type') == 'forecast'):
+            forecasts = textwrap.wrap(dataText.text, width=30)
+        
+    font60 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 60)
     font36 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 36)
     font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
     font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
@@ -34,30 +73,36 @@ try:
     draw = ImageDraw.Draw(Rimage)
 
     # place the weather elements
-    # draw.text((2, 0), 'Blackheath Weather', font = font24, fill = 0)
-    # draw.text((2, 200), "weather here", font = font18, fill = 0)
+    draw.rectangle((0, 0, 300, 89), fill=0)
+    draw.text((30, 30), 'Blackheath Weather', font = font24, fill = epd.GRAY1)
+    #draw.line((0, 90, 300, 90), fill = 0)
+    #draw.line((0, 400, 300, 400), fill = 0)
 
+    draw.text((10, 110), "min", font=font18, fill=epd.GRAY3)
+    draw.text((10, 130), minTemp+"°", font=font24, fill=epd.GRAY4)
+    draw.text((100, 110), "max", font=font18, fill=epd.GRAY3)
+    draw.text((100, 130), maxTemp+"°", font=font24, fill=epd.GRAY4)
+        
+    draw.text((10, 175), "Current temp", font=font18, fill=epd.GRAY3)
+    draw.text((10, 190), str(temp)+"°", font=font60, fill=epd.GRAY4)
+        
+    draw.text((10, 265), "Feels like", font=font18, fill=epd.GRAY3)
+    draw.text((10, 290), str(feelsLike)+"°", font=font24, fill=epd.GRAY4)
 
-    draw.rectangle((130, 20, 274, 56), 'black', 'black')
-    draw.text((130, 20), u'微雪电子', font = font36, fill = epd.GRAY1)
-    draw.text((130, 60), u'微雪电子', font = font36, fill = epd.GRAY2)
-    draw.text((130, 100), u'微雪电子', font = font36, fill = epd.GRAY3)
-    draw.text((130, 140), u'微雪电子', font = font36, fill = epd.GRAY4)
-    draw.line((10, 90, 60, 140), fill = 0)
-    draw.line((60, 90, 10, 140), fill = 0)
-    draw.rectangle((10, 90, 60, 140), outline = 0)
-    draw.line((95, 90, 95, 140), fill = 0)
-    draw.line((70, 115, 120, 115), fill = 0)
-    draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    draw.rectangle((10, 150, 60, 200), fill = 0)
-    draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
+    draw.text((10, 320), "Last checked: "+lastChecked, font=font18, fill=epd.GRAY4)
+        
+    top = 340
+    for forecast in forecasts:
+        draw.text((10, top), forecast, font=font18, fill=epd.GRAY4)
+        top += 20
+        
 
-    
     epd.display_1Gray(epd.getbuffer(Rimage))
-    time.sleep(5)
-    
+    time.sleep(1)
+        
     logging.info("Goto Sleep...")
     epd.sleep()
+    #time.sleep(1800) # 30 minutes
     
 except IOError as e:
     logging.info(e)
